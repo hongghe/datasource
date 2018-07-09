4b23fc186f284bedf485c1ceb5d27d829538f588
hongghe <364930855@qq.com>
2018-07-09 20:59:56 +0800

    utility of time.

---
+++ src/main/java/com/hongghe/datasouce/controller/HomepageController.java
@@ -0,0 +1,14 @@
+package com.hongghe.datasouce.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 入口地址
+ *
+ * @author hongghe 09/07/2018
+ */
+@RestController
+@RequestMapping("/home")
+public class HomepageController {
+}
---
+++ src/test/java/com/hongghe/datasource/PackageInfo.java
@@ -0,0 +1,4 @@
+package com.hongghe.datasource;
+
+public class PackageInfo {
+}
