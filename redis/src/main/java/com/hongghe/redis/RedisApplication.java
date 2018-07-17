0b2eedcaee64ed8be7e160a1e5778b741211890b
hongghe <364930855@qq.com>
2018-07-17 22:56:58 +0800

    redis的使用

---
+++ redis/src/main/java/com/hongghe/redis/RedisApplication.java
@@ -0,0 +1,17 @@
+package com.hongghe.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * startup class of project.
+ *
+ * @author hongghe 07/2018
+ */
+@SpringBootApplication
+public class RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+}
