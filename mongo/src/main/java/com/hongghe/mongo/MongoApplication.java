2fe7bbd0e443211c26211ad6ea4484b3df1fc074
hongghe <364930855@qq.com>
2018-07-20 09:53:36 +0800

    Setting startup class.

--- mongo/src/main/java/com/hongghe/mongo/MongoApplication.java
+++ mongo/src/main/java/com/hongghe/mongo/MongoApplication.java
@@ -2,9 +2,20 @@ package com.hongghe.mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * The startup class of project.
+ *
+ * @author hongghe 20/07/2018
+ */
 @SpringBootApplication
 public class MongoApplication {
+
+	@Bean
+	public MongoProperties mongoProperties() {
+		return new MongoProperties();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoApplication.class, args);
