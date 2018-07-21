8a786501cf79dd3e71787c586ed7302ee904b318
hongghe <364930855@qq.com>
2018-07-21 16:46:04 +0800

    docker部署

--- memcached/src/main/java/com/hongghe/memcached/MemcachedApplication.java
+++ memcached/src/main/java/com/hongghe/memcached/MemcachedApplication.java
@@ -1,10 +1,17 @@
 package com.hongghe.memcached;
 
+import com.hongghe.memcached.config.MemcachedProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MemcachedApplication {
+
+	@Bean
+	public MemcachedProperties memcachedProperties() {
+		return new MemcachedProperties();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemcachedApplication.class, args);
